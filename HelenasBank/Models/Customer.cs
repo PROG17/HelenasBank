0b077966b8a2a708de3d4363edf8ce03f5a6e782
Helena Hedström <31826611+helenahedstrom@users.noreply.github.com>
2018-11-08 10:02:11 +0100

    Added models

---
+++ HelenasBank/Models/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelenasBank.Models
+{
+    public class Customer
+    {
+        public Customer()
+        {
+            Accounts = new List<Account>();
+        }
+        public string Id { get; set; }
+
+        public string OrgNumber { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public string State { get; set; }
+
+        public string Country { get; set; }
+
+        public string Phone { get; set; }
+
+        public List<Account> Accounts { get; set; }
+    }
+}
