97b9adff731bf11695b9a19ccaec31f14e230549
Mimmi Rundlöf <Mimmi.rundlof@yh.nackademin.se>
2018-11-16 16:58:06 +0100

    add transfer function

--- BankTest/BankTest.cs
+++ BankTest/BankTest.cs
@@ -1,4 +1,5 @@
 using HelenasBank.Classes;
+using HelenasBank.Models;
 using HelenasBank.Repo;
 using System;
 using Xunit;
@@ -7,6 +8,55 @@ namespace BankTest
 {
     public class BankTest
     {
+        [Fact]
+        public void Test_Transfer_Amount_Not_Lower_Than_Balance()
+        {
+            //Arrange
+            var transferingAccount = new Account
+            {
+                AccountNo = "123",
+                Balance = 100
+            };
+
+            var receivingAccount = new Account()
+            {
+                AccountNo = "321",
+                Balance = 0
+            };
+
+            //Act
+            var result = transferingAccount.TransferFromAccount(receivingAccount, 100);
+
+            //Assert
+            Assert.True(result);
+
+        }
+
+
+        [Fact]
+        public void Test_Transfer_Betweeen_Accounts()
+        {
+            //Arrange
+            var transferingAccount = new Account
+            {
+                AccountNo = "123",
+                Balance = 100
+            };
+
+            var receivingAccount = new Account()
+            {
+                AccountNo = "321",
+                Balance = 0
+            };
+
+            //Act
+
+            var result = transferingAccount.TransferFromAccount(receivingAccount, 100);
+
+            Assert.True(100 == receivingAccount.Balance && 0 == transferingAccount.Balance);
+            
+
+        }
 
         [Fact]
         public void PerformDeposit_CorrectBalance_True()
---
+++ HelenasBank/ViewModels/TransferViewModel.cs
@@ -0,0 +1,20 @@
+﻿using HelenasBank.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelenasBank.ViewModels
+{
+    public class TransferViewModel
+    {
+        public Account TransferingAccount { get; set; }
+
+        public Account ReceivingAccount { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal Amount { get; set; }
+
+
+    }
+}
